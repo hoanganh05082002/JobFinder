b2d1219832795d697f88069fd5a96155ae2bc3ce
Hoang Anh Nguyen <125590359+hoanganh05082002@users.noreply.github.com>
2023-10-18 15:34:04 +0700

    Revert "NguyenChiLong"

--- JOB_FINDER/NguyenChiLong.cs
+++
@@ -1,6 +0,0 @@
-﻿/*
- * Những kiến thức cần có về GIT , GITHUB , SVN 
- * Các nhóm cần tổ chức làm việc theo nhóm trên GITHUB 
- * Nắm rõ các bước tạo branch , commit , push ,merge , ......
-  
- */
