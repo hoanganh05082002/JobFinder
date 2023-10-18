d0d9ae0084e0c92829697cd0b85055eac4c3be10
hoanganh05082002 <anh05082002@gmail.com>
2023-10-18 15:22:42 +0700

    NguyenChiLong

--- JOB_FINDER/NguyenChiLong.cs
+++ JOB_FINDER/NguyenChiLong.cs
@@ -2,5 +2,5 @@
  * Những kiến thức cần có về GIT , GITHUB , SVN 
  * Các nhóm cần tổ chức làm việc theo nhóm trên GITHUB 
  * Nắm rõ các bước tạo branch , commit , push ,merge , ......
- * Những nhóm G02 , G04 cần rút kinh nghiệm trong việc tổ chức phân chia công việc 
+  
  */
